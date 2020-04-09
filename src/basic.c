b5ef8a05bdafa9193e0f2776524c686d358cf3ec
Ryan Birmingham <birm@rbirm.us>
2020-04-09 01:24:10 -0400

    very slightly more useful garbage

--- src/basic.c
+++ src/basic.c
@@ -9,17 +9,19 @@ int main()
   	uint32 imageWidth, imageLength;
   	uint32 tileWidth, tileLength;
   	uint32 x, y;
-  	tdata_t buf;
+  	//tdata_t buf;
 
   	TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &imageWidth);
   	TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &imageLength);
   	TIFFGetField(tif, TIFFTAG_TILEWIDTH, &tileWidth);
   	TIFFGetField(tif, TIFFTAG_TILELENGTH, &tileLength);
-  	buf = _TIFFmalloc(TIFFTileSize(tif));
+  	//buf = _TIFFmalloc(TIFFTileSize(tif));
+    uint32* buf = (uint32 *)malloc(imageWidth*imageLength * sizeof(uint32));
   	for (y = 0; y < imageLength; y += tileLength)
   	    for (x = 0; x < imageWidth; x += tileWidth)
   		    TIFFReadTile(tif, buf, x, y, 0, 0);
-          printf("%d x %d", imageWidth, imageLength);
+          printf("%d x %d\n", imageWidth, imageLength);
+          printf("%d\n", buf[0]);
   	_TIFFfree(buf);
   	TIFFClose(tif);
   }
